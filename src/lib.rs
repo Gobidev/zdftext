bea2d46aebe90970a4c3b4446de2281745763e32
Adrian Groh <adrian.groh@t-online.de>
2023-10-11 18:28:53 +0200

    Refactor

---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+use html_escape::decode_html_entities;
+use html_parser::Element;
+use owo_colors::{OwoColorize, Rgb};
+use std::fmt::Display;
+
+#[derive(Debug, Clone)]
+pub struct TeletextText {
+    pub text: String,
+    pub fg_color: u32,
+    pub bg_color: u32,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    NoChild,
+    InvalidChild,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::NoChild => write!(f, "Element has no child"),
+            Error::InvalidChild => write!(f, "Element has invalid child"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+fn get_colors_from_classes(classes: &[String]) -> (Option<u32>, Option<u32>) {
+    let mut fg = None;
+    let mut bg = None;
+    for class in classes {
+        if class.starts_with("bc") {
+            bg = u32::from_str_radix(&class.chars().skip(2).collect::<String>(), 16).ok()
+        } else if class.starts_with('c') {
+            fg = u32::from_str_radix(&class.chars().skip(1).collect::<String>(), 16).ok()
+        }
+    }
+    (fg, bg)
+}
+
+/// get separate red, green and blue color values from a combined rgb u32
+fn extract_colors(combined_colors: u32) -> Rgb {
+    let r = ((combined_colors >> 16) & 0xFF) as u8;
+    let g = ((combined_colors >> 8) & 0xFF) as u8;
+    let b = (combined_colors & 0xFF) as u8;
+    Rgb(r, g, b)
+}
+
+impl Display for TeletextText {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let colored_text = self.text.on_color(extract_colors(self.bg_color));
+        let colored_text = colored_text.color(extract_colors(self.fg_color));
+        write!(f, "{colored_text}")
+    }
+}
+
+impl Default for TeletextText {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl TeletextText {
+
+    pub fn new() -> Self {
+        Self { text: "".to_string(), fg_color: 0, bg_color: 0 }
+    }
+
+    pub fn from_element(element: &Element) -> Result<Self, Error> {
+        if element.name == "br" {
+            return Ok(Self::new());
+        }
+        let text: String;
+        let mut fg_color;
+        let bg_color;
+        (fg_color, bg_color) = get_colors_from_classes(&element.classes);
+
+        let child = match element.children.get(0) {
+            Some(child) => child,
+            None => return Ok(Self::new()),
+        };
+
+        match child {
+            html_parser::Node::Text(t) => text = t.clone(),
+            html_parser::Node::Element(e) => {
+                let sub_element = TeletextText::from_element(e)?;
+                (fg_color, _) = get_colors_from_classes(&e.classes);
+                text = sub_element.text;
+            }
+            _ => return Err(Error::InvalidChild),
+        }
+        let fg_color = fg_color.unwrap_or(0xFFFFFF);
+        let bg_color = bg_color.unwrap_or(0x000000);
+        let text = decode_html_entities(&text).to_string();
+        Ok(Self {
+            text,
+            fg_color,
+            bg_color,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,6 @@
-use html_escape::decode_html_entities;
 use html_parser::{Dom, Element, Node};
-use owo_colors::OwoColorize;
-use std::{fmt::Display, io};
-
-#[derive(Debug, Clone)]
-struct Text {
-    text: String,
-    fg_color: u32,
-    bg_color: u32,
-}
-
-impl Display for Text {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let fg_r = ((self.fg_color >> 16) & 0xFF) as u8;
-        let fg_g = ((self.fg_color >> 8) & 0xFF) as u8;
-        let fg_b = (self.fg_color & 0xFF) as u8;
-        let bg_r = ((self.bg_color >> 16) & 0xFF) as u8;
-        let bg_g = ((self.bg_color >> 8) & 0xFF) as u8;
-        let bg_b = (self.bg_color & 0xFF) as u8;
-
-        let colored_text = self.text.on_truecolor(bg_r, bg_g, bg_b);
-        let colored_text = colored_text.truecolor(fg_r, fg_g, fg_b);
-        write!(f, "{colored_text}")
-    }
-}
-
-impl Text {
-    fn from_element(element: Element) -> Self {
-        // todo: rewrite this mess
-        let use_parent_bg: bool;
-        let text_element = if let Some(Node::Element(e)) = element.children.get(0) {
-            use_parent_bg = true;
-            e.clone()
-        } else {
-            use_parent_bg = false;
-            element.clone()
-        };
-
-        let mut fg = 0;
-        let mut bg = 0;
-        let mut text = "".to_string();
-        for class in text_element.classes {
-            if class.starts_with("bc") {
-                let class_base = class.chars().skip(2).collect::<String>();
-                bg = u32::from_str_radix(&class_base, 16).expect("Invalid color");
-            } else if class.starts_with('c') {
-                let class_base = class.chars().skip(1).collect::<String>();
-                fg = u32::from_str_radix(&class_base, 16).expect("Invalid color");
-            }
-        }
-        if use_parent_bg {
-            for class in element.classes {
-                if class.starts_with("bc") {
-                    let class_base = class.chars().skip(2).collect::<String>();
-                    bg = u32::from_str_radix(&class_base, 16).expect("Invalid color");
-                }
-            }
-        }
-
-        if let Some(Node::Text(t)) = text_element.children.get(0) {
-            text = t.clone();
-        }
-
-        let text = decode_html_entities(&text).to_string();
-        Self {
-            text,
-            fg_color: fg,
-            bg_color: bg,
-        }
-    }
-}
+use std::{error::Error, io};
+use zdftext::TeletextText;
 
 fn get_element(input: Option<&Node>) -> &Element {
     if let Node::Element(e) = input.expect("Parser error") {
@@ -79,30 +10,31 @@ fn get_element(input: Option<&Node>) -> &Element {
     }
 }
 
-fn show_dom(dom: Dom) {
+fn show_dom(dom: Dom) -> Result<(), Box<dyn Error>> {
     let content = get_element(dom.children.get(0));
     let body = get_element(content.children.get(1));
     let rows = get_element(body.children.get(1)).children.clone();
-    let ids: Vec<Vec<Text>> = rows
+    let rows_parsed: Vec<Vec<TeletextText>> = rows
         .into_iter()
         .map(|e| match e {
             Node::Element(e) => e
                 .children
                 .iter()
                 .map(|e| match e {
-                    Node::Element(e) => Text::from_element(e.clone()),
+                    Node::Element(e) => TeletextText::from_element(&e.clone()).unwrap(),
                     _ => panic!("Parser error"),
                 })
                 .collect(),
             _ => panic!("Parser error"),
         })
         .collect();
-    for line in ids {
-        for text in line {
+    for row in rows_parsed {
+        for text in row {
             print!("{text}");
         }
         println!();
     }
+    Ok(())
 }
 
 fn main() {
@@ -125,7 +57,7 @@ fn main() {
             number = number_before.clone();
             continue;
         };
-        show_dom(parsed_response);
+        drop(show_dom(parsed_response));
         println!("Enter number: ");
 
         number_before = number;
